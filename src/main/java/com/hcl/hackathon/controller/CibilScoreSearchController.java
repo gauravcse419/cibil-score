3c388de827fa7aa73bfdadc31ff7e01368432135
apallapu_java <ankamma.java@gmail.com>
2021-07-10 15:12:03 +0530

    done

---
+++ src/main/java/com/hcl/hackathon/controller/CibilScoreSearchController.java
@@ -0,0 +1,44 @@
+package com.hcl.hackathon.controller;
+
+import com.hcl.hackathon.model.CibilScoreDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@Tag(name = "Cibil Score", description = "The Cibil Score API")
+public class CibilScoreSearchController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * Find credit score by pan number.
+     *
+     * @param panNumber the panNumber
+     * @return the list
+     */
+    @Operation(summary = "Find the cibil scores by pan number", description = "Returns a Cibil score for customer ", tags = { "Cibil Score" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation",
+                    content = @Content(schema = @Schema(implementation = CibilScoreDetails.class))),
+            @ApiResponse(responseCode = "404", description = "PanNumber not found") })
+    @GetMapping(value = "/cibilscore/{panNumber}", produces = { "application/json" })
+    public CibilScoreDetails findCibilScoreByPanNumber(
+            @Parameter(description="Id of the cibil score to be obtained. Cannot be empty.", required=true)
+            @PathVariable String panNumber) {
+        logger.debug("Started CibilScoreSearchController.findCibilScoreByPanNumber {} ",panNumber);
+        //return userService.findOrdersByUserId(userId);
+        return null;
+    }
+}
---
+++ src/main/java/com/hcl/hackathon/model/CibilScoreDetails.java
@@ -0,0 +1,25 @@
+package com.hcl.hackathon.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class CibilScoreDetails {
+    @Schema(description = "name",
+            example = "Johan", required = false)
+    private String name;
+
+    @Schema(description = "cibilScore",
+            example = "850", required = false)
+    private Double cibilScore;
+
+    @Schema(description = "panCard",
+            example = "BIH8789088", required = false)
+    private Double panCard;
+
+
+
+
+}
